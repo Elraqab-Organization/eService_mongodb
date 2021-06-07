bd86966114b8322d5150b44be514338efe3b0b3d
MarwanMostafa Taha <tmarawan11@gmail.com>
2021-06-07 06:03:00 +0300

    modified server file and customized stuff for herouku

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const userRouter = require('./api/controllers/user_controller');
 app.use(express.json())
 app.use('/users', userRouter)
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+
+    console.log("started at port")
+})
+
+
+module.exports = { app }
+
+
 // exports.api = functions.https.onRequest(app)
 
 // // To handle "Function Timeout" exception
