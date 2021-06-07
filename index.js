10d5cf8fe97a93a4e59ee617bf3210172d68e5cb
MarwanMostafa Taha <tmarawan11@gmail.com>
2021-06-07 06:35:21 +0300

    changes to indexjs

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const app = express();
 const userRouter = require('./api/controllers/user_controller');
+const router = express.Router()
+const userModel = require('./api/models/user_model')
 
 app.use(express.json())
 app.use('/users', userRouter)
 
+router.get('/', async(req, res, next) => {
+    try {
+        const result = await userModel.get()
+        return res.json(result)
+    } catch (e) {
+        return next(e)
+    }
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 
