42f474e5aa1dff1a4ac4bdbf99a4a40e6f470175
MarwanMostafa Taha <tmarawan11@gmail.com>
2021-06-09 23:30:50 +0300

    deploy attempt

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const uri = process.env.MONGO_DB_URI || 'mongodb+srv://Ulan:123@cluster1.qcpvo.mongodb.net/Students?retryWrites=true&w=majority'
 
-mongoose.connect("mongodb+srv://ahmadelraqab:123123258Aa@cluster0.whjky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect(uri, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
